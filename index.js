573f9905ce75e46d85c6685247dbdf80ca64c738
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-12 11:51:05 +0530

    test add

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 // Add the Calculator APIs
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname))
-app.use(express.json())
+app.use(express.static(__dirname));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/main.html"));
 });
 
 //your code here
 
-app.post('/add', (req, res)=>{
-  const {num1, num2} = req.body
-  if(typeof num1 != Number || typeof num2 != Number)
-  return res.send({status: "error"})
-  res.send({          
-    status: "success",  
-    message: "the sum of given two numbers",  
-    sum: num1+num2
-    })
-})
+app.post("/add", (req, res) => {
+  const { num1, num2 } = req.body;
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return res.send({ status: "error" });
+  }
+  res.send({
+    status: "success",
+    message: "the sum of given two numbers",
+    sum: num1 + num2,
+  });
+});
 
 module.exports = app;
