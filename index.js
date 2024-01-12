85c2d49627aa07796e80d49a73a12f3cbafaff3f
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-12 13:45:59 +0530

    test add

--- index.js
+++ index.js
@@ -17,18 +17,24 @@ app.get("/", (req, res) => {
 app.post("/add", (req, res) => {
   const { num1, num2 } = req.body;
   if (!num2 || !num1) {
-    return res.send({ status: "error" , message:"numbers not found"});
+    return res.send({ status: "error", message: "numbers not found" });
   }
   if (typeof num1 !== "number" || typeof num2 !== "number") {
-    return res.send({ status: "error" , message:"Invalid data types"});
+    return res.send({ status: "error", message: "Invalid data types" });
   }
-  if (Math.abs(num1) > 1000000 || Math.abs(num2) > 1000000){
-    return res.send({ status: "error" , message:"Overflow"});
+  if (Math.abs(num1) > 1000000 || Math.abs(num2) > 1000000) {
+    return res.send({ status: "error", message: "Overflow" });
+  }
+  const result = num1 + num2;
+  if (Math.abs(result) > 1000000) {
+    return res.send({ status: "error", message: "Overflow" });
+  } else if (Math.abs(result) < -1000000) {
+    return res.send({ status: "error", message: "Undeflow" });
   }
   res.send({
     status: "success",
     message: "the sum of given two numbers",
-    sum: num1 + num2,
+    sum: result,
   });
 });
 
